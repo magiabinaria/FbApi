91d9a4692ad72efcb5c1f9eb5f0e44cd2e15db81
Gerardo Martinez <diryess@gmail.com>
2012-11-17 03:39:58 -0600

    Agregado Api Key
    Agregamos la Api Key

---
+++ config/facebook.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * Get an api_key and secret from facebook and fill in this content.
+	 * save the file to app/config/facebook.php
+	 *
+	 * http://www.facebook.com/developers/
+	 */
+	$config = array(
+		'Facebook' => array(
+			'appId' => '393258000722626',
+			'apiKey' => '393258000722626',
+			'secret' => 'b4c7004c9095637baa5e5155ee2463db',
+			'cookie' => true,
+			'userModel' => 'MyPlugin.MyUserModel'
+		)
+	);
